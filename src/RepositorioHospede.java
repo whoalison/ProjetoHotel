01fbf3452a740b7d7026edbee63ee195e1c14844
Nicole Oliveira <nicoleolsbcc@gmail.com>
2024-01-31 18:37:55 -0300

    Modificações nos repositórios

--- src/RepositorioHospede.java
+++ src/RepositorioHospede.java
@@ -1,28 +1,28 @@
-import java.util.List;
+import java.util.ArrayList;
 
 public class RepositorioHospede {
-    private List<Hospede> hospedes;
+    private ArrayList<Hospede> hospedes;
 
-    public RepositorioHospede(List<Hospede> hospedes) {
+    public RepositorioHospede(ArrayList<Hospede> hospedes) {
         this.hospedes = hospedes;
     }
 
-    public void cadastrarHospede (Hospede hospede) {
+    public void cadastrarHospede(Hospede hospede) {
         hospedes.add(hospede);
         // cadastramento de novo hospede na lista de hospedes do sistema
     }
 
-    public List<Hospede> getAllHospedes() {
+    public ArrayList<Hospede> getAllHospedes() {
         return hospedes;
         // retorna a lista completa de hospedes
     }
 
     public void removerHospede(int id) {
         hospedes.removeIf(hospede -> hospede.getId() == id);
-        //remove um hospede da lista de hospedes do sistema
+        // remove um hospede da lista de hospedes do sistema
     }
 
-    public Hospede getHospedePorId (int id) {
+    public Hospede getHospedePorId(int id) {
         for (Hospede hospede : hospedes) {
             if (hospede.getId() == id) {
                 return hospede;
--- src/RepositorioReserva.java
+++ src/RepositorioReserva.java
@@ -1,26 +1,28 @@
-import java.util.List;
+import java.util.ArrayList;
 
 public class RepositorioReserva {
-    private List<Reserva> reservas;
+    private ArrayList<Reserva> reservas;
 
-    public RepositorioReserva(List<Reserva> reservas) {
-        this.reservas = reservas;
+    public RepositorioReserva() {
+        this.reservas = new ArrayList<>();
     }
 
-    public void adicionarReserva (Reserva reserva) {
+    public void adicionarReserva(Reserva reserva) {
         reservas.add(reserva);
         // adiciona uma reserva na lista de reservas do sistema
     }
-    public List<Reserva> getAllReserva () {
+
+    public ArrayList<Reserva> getAllReserva() {
         return reservas;
         // retorna a lista completa de reservas
     }
 
     public void removerReserva(int id) {
         reservas.removeIf(reserva -> reserva.getId() == id);
-        //remove uma reserva da lista de reservas do sistema
+        // remove uma reserva da lista de reservas do sistema
     }
-    public Reserva getReservaPorId (int id) {   // id: numero do cadastro de cada hospede "comanda"
+
+    public Reserva getReservaPorId(int id) {
         for (Reserva reserva : reservas) {
             if (reserva.getId() == id) {
                 return reserva;
